80cc6a2972c3c3e60ffa35b93482850d53cb0d16
Sazan-Mahbub <smahbub@andrew.cmu.edu>
2024-09-16 03:00:57 +0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RAG_IM
-Retrieval Augmented Generation of Interpretable Models
+This repository contains the official implementation of RAG-IM (Retrieval Augmented Generation of Interpretable Models). 
+
+## Download raw data
+Please download the original dataset from [here](https://physionet.org/content/mimiciv/3.0/). The following files are required:
+- HOSP.procedures_icd.csv
+- HOSP.d_icd_procedures.csv
+- HOSP.d_icd_diagnoses.csv
+- HOSP.d_labitems.csv
+- ED.diagnosis.csv
+- labevents.csv
+
+## Run the experiments (training and testing)
+> bash run_experiment.sh
+
+## Only test the already trained model
+> bash run_test_only.sh
+
--- preprocess_step1.py
+++ preprocess_step1.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
 data_dir = './data_v2'
-
-
-# In[2]:
 
 
 allowed_icd_codes = {'02HV33Z', '5A1D70Z', '3E0G76Z', '5A1D60Z', '0DJ08ZZ', '0BH17EZ', '0W9G3ZZ', 'B211YZZ', '02H633Z', '5A1945Z', '0W9G3ZX', '0DJD8ZZ', '5A1955Z', '009U3ZX', '3E0436Z', '5A1D00Z', '3E04305', '0DB68ZX', '5A1935Z', '10E0XZZ', '4A023N6', '4A023N7', '3E0H76Z', '0W9930Z', 'B548ZZA', 'GZB0ZZZ', '5A2204Z', '5A1221Z', '0DB98ZX', 'GZB2ZZZ', '0JH63XZ', '0DH63UZ', '027034Z', '0W9B30Z', '3E1M39Z', '0F798DZ', '0W3P8ZZ', 'B2111ZZ', '0BJ08ZZ', '07DR3ZX', '10D00Z1', '0T25X0Z', '0CJS8ZZ', 'BF10YZZ', '0FC98ZZ', '0JBR0ZZ', '0DBN8ZX', '30233S1', '3E03305', '0W9G30Z'}
@@ -21,30 +12,14 @@ HOSP = HOSP[HOSP['icd_code'].isin(allowed_icd_codes)]
 subject_id_set_hosp = HOSP['subject_id'].unique().tolist()
 subject_id_set_hosp = set(subject_id_set_hosp)
 
-# subject_id_set_hosp
-
-
-# In[3]:
-
 
 diagnosis = pd.read_csv(filepath_or_buffer=f'{data_dir}/ED.diagnosis.csv')
 diagnosis = diagnosis[diagnosis['icd_version'] == 10]
 subject_id_set = diagnosis['subject_id'].unique().tolist()
 subject_id_set = set(subject_id_set)
 
-# subject_id_set
-
-
-# In[4]:
-
 
 filtered_subjects_set = subject_id_set.intersection(subject_id_set_hosp)
-
-# len(filtered_subjects_set)
-
-
-# In[5]:
-
 
 filtered_labevents = open(f'{data_dir}/filtered_labevents__all_icd_code.csv', 'w')
 
@@ -79,21 +54,6 @@ filtered_labevents.close()
 
 print('Total:', cnt, '\tTotal success:', cnt_success)
 print('Done')
-
-
-# In[1]:
-
-
-# 37489622 / 118171367
-
-
-# In[2]:
-
-
-# 37489622 / 2
-
-
-# In[8]:
 
 
 # filtered_labevents = open('filtered_labevents__all_icd_code_split1.csv', 'w')
@@ -132,40 +92,6 @@ print('Done')
 
 # print('Total:', cnt, '\tTotal success:', cnt_success)
 # print('Done')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
