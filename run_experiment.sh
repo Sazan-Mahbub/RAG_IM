5a1d1bcac5a4ec65882cb45ebbf06e473d7c0f47
Sazan-Mahbub <smahbub@andrew.cmu.edu>
2024-09-16 02:36:54 +0400

    init commit

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,4 +1,5 @@
-python preprocess.py &> logs/preprocess.log
+python preprocess_step1.py &> logs/preprocess_step1.log
+python preprocess_step2.py &> logs/preprocess_step2.log
 python prepare_logreg.py &> logs/prepare_logreg.log
 python train.py &> logs/train.log
 python inference_all.py &> logs/inference_all.log
--- run_test_only.sh
+++ run_test_only.sh
@@ -1,3 +1,4 @@
-python preprocess.py &> logs/preprocess.log
+python preprocess_step1.py &> logs/preprocess_step1.log
+python preprocess_step2.py &> logs/preprocess_step2.log
 python prepare_logreg.py &> logs/prepare_logreg.log
 python inference_all.py &> logs/inference_all.log
